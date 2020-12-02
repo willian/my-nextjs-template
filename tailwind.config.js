6a00d55f7a98b4b85d4eb2ac11a38e3117594841
Willian Fernandes <willian@willianfernandes.com.br>
2020-12-02 16:50:16 -0300

    Update configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      screens: {
+        standalone: { raw: '(display-mode: standalone)' },
+      },
+      spacing: {
+        22: '5.5rem',
+      },
+    },
   },
-  variants: {
-    extend: {},
-  },
+  variants: {},
   plugins: [],
 }
